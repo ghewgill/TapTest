3044bfa80c9b52dda1a7215a748f8fa7bb39d343
Greg Hewgill <greg@hewgill.com>
2012-11-26 08:36:32 +1300

    Initial Commit

---
+++ TapTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TapTest
+//
+//  Created by Greg Hewgill on 26/11/12.
+//  Copyright (c) 2012 Greg Hewgill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ TapTest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TapTest
+//
+//  Created by Greg Hewgill on 26/11/12.
+//  Copyright (c) 2012 Greg Hewgill. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
