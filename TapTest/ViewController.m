db2bdd94fc22dd6696e3514f3ea6b0fea64d7d8c
Greg Hewgill <greg@hewgill.com>
2012-11-26 08:43:09 +1300

    add sample code for tap test

--- TapTest/ViewController.m
+++ TapTest/ViewController.m
@@ -12,18 +12,34 @@
 
 @end
 
-@implementation ViewController
+@implementation ViewController {
+    UILabel *info;
+    UIGestureRecognizer *tapper;
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    info = [[UILabel alloc] initWithFrame:CGRectMake(100, 100, 200, 20)];
+    [self.view addSubview:info];
+    
+    tapper = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleTap:)];
+    [self.view addGestureRecognizer:tapper];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)handleTap:(UIGestureRecognizer *)sender
+{
+    CGPoint p = [sender locationInView:self.view];
+    NSLog(@"tap %g,%g", p.x, p.y);
+    info.text = [NSString stringWithFormat:@"tap %ld %g,%g", time(NULL), p.x, p.y];
 }
 
 @end
